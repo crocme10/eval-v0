ac71f96f959ceab32b7a96c675e9b9eb52bf36db
crocme10 <matt@area403.org>
2023-07-25 22:25:20 +0200

    Add variables for hero section

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,25 @@
-<script setup>
-import { ref } from 'vue';
-
-defineProps({
-  msg: String,
-});
-
-const count = ref(0);
+<script>
+export default {
+  name: 'HelloWorld',
+  setup() {
+    const heroTitle = 'Les Velos Parisiens';
+    const heroSubtitle = 'where bikes get fixed';
+    return {
+      heroTitle,
+      heroSubtitle,
+    };
+  },
+};
 </script>
 
 <template>
-<div class="bg-cover"
+  <!-- template based on https://tailwindcomponents.com/component/centered-hero-header -->
+  <div class="bg-cover"
     style="background-image: url('https://images.pexels.com/photos/57690/pexels-photo-57690.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'); height: 400px">
     <nav id="nav"
       class="fixed inset-x-0 top-0 flex flex-row justify-between
       z-10 text-white bg-transparent font-text">
-    <div class="p-4">
+      <div class="p-4">
         <div class="font-extrabold tracking-widest text-xl">
           <a href="#" class="transition duration-500 hover:text-indigo-500">GARBAGE</a>
         </div>
@@ -64,12 +69,11 @@ const count = ref(0);
         <div class="mx-2 text-center">
           <h1 class="text-gray-100 font-extrabold font-text
             text-4xl xs:text-5xl md:text-6xl">
-            <span class="text-white">Right</span> Place To
+            <span class="text-white">{{ heroTitle }}</span>
           </h1>
           <h2 class="text-gray-200 font-extrabold font-text
             text-3xl xs:text-4xl md:text-5xl leading-tight">
-            Get a <span class="text-white">Better</span>
-            and <span class="text-white">Professionals</span> Design
+            <span class="text-white">{{ heroSubtitle }}</span>
           </h2>
           <div class="inline-flex">
             <button class="p-2 my-5 mx-2 bg-indigo-700 hover:bg-indigo-800
